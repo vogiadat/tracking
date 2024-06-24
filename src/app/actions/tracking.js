be2b9c1520bba2e8690caebf4a2d4e271f148860
vogiadat-window <contact.vgdat@gmail.com>
2024-06-24 21:26:14 +0700

    styles: update ui

--- src/app/actions/tracking.js
+++ src/app/actions/tracking.js
@@ -2,13 +2,13 @@
 import { catching } from '@/config/const'
 
 export const getTrackingById = async (trackingNumber) => {
-  const res = await fetch(`http://localhost:3000/api/tracking/${trackingNumber}`, {
+  const res = await fetch(process.env.TRACKING_ENV + `/api/tracking/${trackingNumber}`, {
     next: { tags: [catching.GET_TRACKING_BY_ID] }
   })
   return await res.json()
 }
 
 export const fetchTracking = async () => {
-  const res = await fetch('http://localhost:3000/api/tracking', { next: { tags: [catching.GET_TRACKING_BY_ID] } })
+  const res = await fetch(process.env.TRACKING_ENV + 'http://localhost:3000/api/tracking', { next: { tags: [catching.GET_TRACKING_BY_ID] } })
   return await res.json()
 }
