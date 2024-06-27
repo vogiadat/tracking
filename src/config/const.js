dde09c4a52cb113893d2e826d6af0ad54a7415f7
vogiadat-window <contact.vgdat@gmail.com>
2024-06-27 17:42:15 +0700

    hotfix: for deploy

--- src/config/const.js
+++ src/config/const.js
@@ -2,4 +2,4 @@ export const catching = {
   GET_TRACKING_BY_ID: 'GET_TRACKING_BY_ID'
 }
 
-export const compileURL = (url) => `http://localhost:3000/${url}`
+export const compileURL = (url) => `http://103.98.160.26:3000/${url}`
