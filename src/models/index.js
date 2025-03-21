ab1f9a99a50705e080f36ccf4771daf97776f86c
rolle_dev <89302745+toanhhg123@users.noreply.github.com>
2025-03-21 23:59:45 +0700

    fix: synchronize Tracking and TrackingItem models

--- src/models/index.js
+++ src/models/index.js
@@ -3,5 +3,6 @@ const TrackingItem = require('./TrackingItem')
 
 TrackingItem.belongsTo(Tracking, { as: 'tracking', foreignKey: 'trackingId' })
 Tracking.hasMany(TrackingItem, { as: 'trackingItems', foreignKey: 'trackingId', sourceKey: 'id' })
-// TrackingItem.sync({ alter: true })
+Tracking.sync({ alter: true })
+TrackingItem.sync({ alter: true })
 module.exports = { Tracking, TrackingItem }
